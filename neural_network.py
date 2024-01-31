5a5d19cc177aac2d6e4df653dfda9df4009f18ab
Vedansh Arya <116213735+vedu-a@users.noreply.github.com>
2024-01-30 20:31:25 -0500

    Create neural_network.py

---
+++ neural_network.py
@@ -0,0 +1,112 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in 
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+
+import matplotlib.pyplot as plt # plotting library
+%matplotlib inline
+
+
+from keras.models import Sequential
+from keras.layers import Dense , Activation, Dropout
+from keras.optimizers import Adam ,RMSprop
+from keras import  backend as K
+
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+
+from subprocess import check_output
+#print(check_output(["ls", "../input"]).decode("utf8"))
+
+
+# Any results you write to the current directory are saved as output
+
+# import dataset
+from keras.datasets import mnist
+
+
+# load dataset
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+
+
+
+# count the number of unique train labels
+unique, counts = np.unique(y_train, return_counts=True)
+print("Train labels: ", dict(zip(unique, counts)))
+
+
+
+# count the number of unique test labels
+unique, counts = np.unique(y_test, return_counts=True)
+print("\nTest labels: ", dict(zip(unique, counts)))
+
+# sample 25 mnist digits from train dataset
+indexes = np.random.randint(0, x_train.shape[0], size=25)
+images = x_train[indexes]
+labels = y_train[indexes]
+
+
+# plot the 25 mnist digits
+plt.figure(figsize=(5,5))
+for i in range(len(indexes)):
+    plt.subplot(5, 5, i + 1)
+    image = images[i]
+    plt.imshow(image, cmap='gray')
+    plt.axis('off')
+    
+plt.show()
+plt.savefig("mnist-samples.png")
+plt.close('all')
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.utils import to_categorical, plot_model
+
+# compute the number of labels
+num_labels = len(np.unique(y_train))
+
+# convert to one-hot vector
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+# image dimensions (assumed square)
+image_size = x_train.shape[1]
+input_size = image_size * image_size
+input_size
+
+# resize and normalize
+x_train = np.reshape(x_train, [-1, input_size])
+x_train = x_train.astype('float32') / 255
+x_test = np.reshape(x_test, [-1, input_size])
+x_test = x_test.astype('float32') / 255
+
+# network parameters
+batch_size = 128
+hidden_units = 256
+dropout = 0.45
+
+# model is a 3-layer MLP with ReLU and dropout after each layer
+model = Sequential()
+model.add(Dense(hidden_units, input_dim=input_size))
+model.add(Activation('relu'))
+model.add(Dropout(dropout))
+model.add(Dense(hidden_units))
+model.add(Activation('relu'))
+model.add(Dropout(dropout))
+model.add(Dense(num_labels))
+model.add(Activation('softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy', 
+              optimizer='adam',
+              metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=20, batch_size=batch_size)
+
+loss, acc = model.evaluate(x_test, y_test, batch_size=batch_size)
+print("\nTest accuracy: %.1f%%" % (100.0 * acc))
